19757493f3138a3b8e7a05e71890ca1f4667ad81
11114E <163570670+11114E@users.noreply.github.com>
2024-10-20 22:24:34 +0400

    Add files via upload

--- track_balances.sh
+++ track_balances.sh
@@ -6,19 +6,16 @@ LOG_FILE="/root/track_balances.log"
 cd /root/ceremonyclient/node || exit 1  # Exit if directory change fails
 
 # Fetch balance and peer ID
-PEER_ID=$(./ceremonyclientnode-1.4.21.1-linux-amd64 -peer-id | grep "Peer ID" | awk '{print $3}')
-BALANCE=$(/ceremonyclientnode-1.4.21.1-linux-amd64 -balance | grep "Unclaimed balance" | awk '{print $3}')
-
-# Get the current date and time
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
-
-# Get the hostname of the machine
+PEER_ID=$(./node-2.0.0.3-linux-amd64 -peer-id | grep "Peer ID" | awk '{print $3}')
+BALANCE=$(./node-2.0.0.3-linux-amd64 -balance | grep "Unclaimed balance" | awk '{print $3}')
 HOSTNAME=$(hostname)
+
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 # Log peer ID, balance, and hostname
 echo "[$DATE] Peer ID: $PEER_ID, Balance: $BALANCE, Hostname: $HOSTNAME" >> $LOG_FILE
 
-# Send the data to the Flask server, including the hostname
+# Send the data to the Flask server
 curl -X POST -H "Content-Type: application/json" \
 -d "{\"peer_id\":\"$PEER_ID\", \"balance\":\"$BALANCE\", \"timestamp\":\"$DATE\", \"hostname\":\"$HOSTNAME\"}" \
-http://192.168.20.210:5000/update_balance >> $LOG_FILE 2>&1
+http://0.0.0.0:5000/update_balance >> $LOG_FILE 2>&1
